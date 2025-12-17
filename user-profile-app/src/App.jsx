4bf6e5186582fd46f8012902bae416065be06b38
NasreenA02 <nassu324@gmail.com>
2025-12-17 21:46:41 +0530

    parent child components

---
+++ user-profile-app/src/App.jsx
@@ -0,0 +1,12 @@
+import UserProfile from "./UserProfile";
+
+function App() {
+  return (
+    <div>
+      <h1>Parent to Child Props Example</h1>
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
