ba2711dfeb54cdc866f93329bd9a2b7e39fa1bb0
NasreenA02 <nassu324@gmail.com>
2025-12-17 21:10:52 +0530

    counter app

---
+++ my-counter-app/src/Counter.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div>
+      <h3>{count}</h3>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+}
